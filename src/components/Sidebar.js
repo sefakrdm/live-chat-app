482af7efc24d2638dbacfd95e1fcc063e50079c6
Sefa Karademir <sefakrdm@gmail.com>
2023-06-09 16:33:59 +0300

    Responsive +3

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,17 +6,21 @@ import { MdOutlineArrowForwardIos, MdOutlineArrowBackIosNew } from 'react-icons/
 import { ChatContext } from '../contexts/ChatContext'
 
 export default function Sidebar() {
-  const [sidebar,setSidebar] = useState(true);
+  const [sidebarActive,setSidebarActive] = useState(true);
   const {data} = useContext(ChatContext)
 
   useEffect(() => {
-    setSidebar(false)
+    if(data.chatId === "null") {
+      setSidebarActive(true)
+    } else {
+      setSidebarActive(false)
+    }
   },[data.chatId])
 
   return (
     <>
-      {sidebar ? <MdOutlineArrowForwardIos size={45} className="fixed z-20 block md:hidden top-3 right-3 bg-blue-500 rounded-md p-2" onClick={()=>setSidebar(!sidebar)}/>:<MdOutlineArrowBackIosNew size={45} className="fixed z-20 block md:hidden top-3 right-3 bg-blue-500 rounded-md p-2" onClick={()=>setSidebar(!sidebar)}/>}
-      <div className={`bg-slate-700 flex-auto md:w-1/4 w-full fixed h-full top-0 left-0 border-r-2 border-black z-10 ${sidebar?'block':'hidden'}`}>
+      {sidebarActive ? <MdOutlineArrowForwardIos size={45} className="fixed z-20 block md:hidden top-3 right-3 bg-blue-500 rounded-md p-2" onClick={()=>setSidebarActive(!sidebarActive)}/>:<MdOutlineArrowBackIosNew size={45} className="fixed z-20 block md:hidden top-3 right-3 bg-blue-500 rounded-md p-2" onClick={()=>setSidebarActive(!sidebarActive)}/>}
+      <div className={`bg-slate-700 md:block flex-auto md:w-1/4 w-full fixed h-full top-0 left-0 border-r-2 border-black z-10 ${sidebarActive ?'block':'hidden'}`}>
         <Navbar />
         <Search />
         <Chats />
